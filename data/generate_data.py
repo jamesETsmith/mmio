d96ae514e912b07b705c23f00e3f84551e8639de
James Smith <james.smith9113@gmail.com>
2023-03-29 15:00:23 -0400

    Second commit

---
+++ data/generate_data.py
@@ -0,0 +1,13 @@
+from scipy.sparse import random
+from scipy.io import mmwrite
+
+
+def main():
+    dens = 0.2
+    for n in range(2,5):
+
+        m = random(10**n, 10**n, dens)
+        mmwrite(f"d={dens}_n={n}.mtx", m)
+
+if __name__ == "__main__":
+    main()
