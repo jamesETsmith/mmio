ed27755920d350919fbb9d9f8783a9def11501b6
James Smith <james.smith9113@gmail.com>
2023-05-20 13:56:42 -0400

    OpenMP version of reader is working

--- scratch/io.cpp
+++ scratch/io.cpp
@@ -78,24 +78,39 @@ int main(int argc, char** argv) {
   from_chars_range(toks_header[0], m);
   from_chars_range(toks_header[1], n);
   from_chars_range(toks_header[2], nnz);
-  // std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(), m);
-  // std::from_chars(toks[1].data(), toks[1].data() + toks[1].size(), n);
-  // std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(), nnz);
 
   std::vector<size_t> e_in(nnz), e_out(nnz);
   std::vector<double> e_weight(nnz);
+  std::vector<size_t> line_start(nnz);
+  std::vector<size_t> line_end(nnz);
 
   start = end + 1;
   end = str.find("\n", start);
 
-  size_t data_line = 0;
+  // size_t data_line = 0;
+  auto find_time_start = std::chrono::steady_clock::now();
+  for (size_t i = 0; i < nnz; i++) {
+    line_start[i] = start;
+    line_end[i] = end;
+    // std::cout << start << " " << end << std::endl;
+
+    start = end + 1;
+    end = str.find("\n", start);
+  }
+  double find_time = std::chrono::duration<double>(
+                         std::chrono::steady_clock::now() - find_time_start)
+                         .count();
+
+  line_end.back() = filesize;
+  // std::cout << line_end.back() << std::endl;
 
   // Read the rest of the data
   double split_time = 0;
   double from_chars_time = 0;
-  while (end != std::string::npos) {
+#pragma omp parallel for reduction(+ : split_time, from_chars_time)
+  for (size_t i = 0; i < nnz; i++) {
     // =( std::string_view initialization
-    std::string_view sv(&str[0] + start, end - start);
+    std::string_view sv(&str[0] + line_start[i], line_end[i] - line_start[i]);
     auto st_i = std::chrono::steady_clock::now();
     std::vector<std::string_view> toks = split(sv);
     split_time +=
@@ -103,27 +118,22 @@ int main(int argc, char** argv) {
             .count();
 
     auto fc_i = std::chrono::steady_clock::now();
-    from_chars_range(toks[0], e_in[data_line]);
-    from_chars_range(toks[1], e_out[data_line]);
-    from_chars_range(toks[2], e_weight[data_line]);
-
-    // std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(),
-    // e_in[data_line]); std::from_chars(toks[1].data(), toks[1].data() +
-    // toks[1].size(),                    e_out[data_line]);
-    // std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(),
-    // e_weight[data_line]);
+    from_chars_range(toks[0], e_in[i]);
+    from_chars_range(toks[1], e_out[i]);
+    from_chars_range(toks[2], e_weight[i]);
     from_chars_time +=
         std::chrono::duration<double>(std::chrono::steady_clock::now() - fc_i)
             .count();
-
-    data_line++;
-    start = end + 1;
-    end = str.find("\n", start);
   }
+
+  // Logging
   std::cout << "Number of edges " << e_in.size() << std::endl;
   std::cout << "First edge: " << e_in[0] << " " << e_out[0] << " "
             << e_weight[0] << std::endl;
+  std::cout << "Last edge: " << e_in.back() << " " << e_out.back() << " "
+            << e_weight.back() << std::endl;
 
+  std::cout << "Find time            (cumulative): " << find_time << std::endl;
   std::cout << "Split time           (cumulative): " << split_time << std::endl;
   std::cout << "std::from_chars time (cumulative): " << from_chars_time
             << std::endl;
