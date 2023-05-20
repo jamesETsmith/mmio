415bbbc3fc2b2206126cd0135b5f73107050e1a3
James Smith <james.smith9113@gmail.com>
2023-05-20 12:33:58 -0400

    Working version of matrix market reader. Only slightly faster than mmio.c

--- scratch/io.cpp
+++ scratch/io.cpp
@@ -1,4 +1,5 @@
 #include <charconv>
+#include <chrono>
 #include <cstdint>
 #include <cstdio>
 #include <fstream>
@@ -7,7 +8,8 @@
 #include <vector>
 
 // https://godbolt.org/z/5ajbvnsGf
-std::vector<std::string_view> split(std::string_view sv, char separator = ' ') {
+inline std::vector<std::string_view> split(std::string_view sv,
+                                           char separator = ' ') {
   std::vector<std::string_view> res;
   size_t start = 0, end = 0;
   for (size_t i = 0; i < sv.size(); i++) {
@@ -34,13 +36,20 @@ std::vector<std::string_view> split(std::string_view sv, char separator = ' ') {
   return res;
 }
 
-int main(int argc, char** argv) {
-  // FILE* fp = std::fopen("tmp.mtx", "r");
-  // std::fstream file("tmp.mtx");
-  // std::stringstream ss;
-  // ss << file.rdbuf();
+template <typename T>
+inline void from_chars_range(std::string_view sv, T& result) {
+  std::from_chars(sv.data(), sv.data() + sv.size(), result);
+}
 
-  std::ifstream fin("tmp.mtx", std::ios::binary);
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Wrong command line args. Use './io matrix.mtx'" << std::endl;
+    exit(1);
+  }
+  std::string filename(argv[1]);
+  std::cout << "Parsing " << filename << std::endl;
+
+  std::ifstream fin(filename, std::ios::binary);
   if (!fin) return 0;
 
   fin.seekg(0, std::ios::end);
@@ -54,13 +63,24 @@ int main(int argc, char** argv) {
   size_t start = 0, end = 0;
   end = str.find("\n");
 
+  // skip comments
+  while (str[start] == '%') {
+    start = end + 1;
+    end = str.find("\n", start);
+  }
+
+  // Meta info
+
   size_t m = 0, n = 0, nnz = 0;
   std::string_view sv_header(&str[0] + start, end - start);
-  std::vector<std::string_view> toks = split(sv_header);
+  std::vector<std::string_view> toks_header = split(sv_header);
 
-  std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(), m);
-  std::from_chars(toks[1].data(), toks[1].data() + toks[1].size(), n);
-  std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(), nnz);
+  from_chars_range(toks_header[0], m);
+  from_chars_range(toks_header[1], n);
+  from_chars_range(toks_header[2], nnz);
+  // std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(), m);
+  // std::from_chars(toks[1].data(), toks[1].data() + toks[1].size(), n);
+  // std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(), nnz);
 
   std::vector<size_t> e_in(nnz), e_out(nnz);
   std::vector<double> e_weight(nnz);
@@ -71,22 +91,42 @@ int main(int argc, char** argv) {
   size_t data_line = 0;
 
   // Read the rest of the data
+  double split_time = 0;
+  double from_chars_time = 0;
   while (end != std::string::npos) {
     // =( std::string_view initialization
     std::string_view sv(&str[0] + start, end - start);
+    auto st_i = std::chrono::steady_clock::now();
     std::vector<std::string_view> toks = split(sv);
+    split_time +=
+        std::chrono::duration<double>(std::chrono::steady_clock::now() - st_i)
+            .count();
 
-    std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(),
-                    e_in[data_line]);
-    std::from_chars(toks[1].data(), toks[1].data() + toks[1].size(),
-                    e_out[data_line]);
-    std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(),
-                    e_weight[data_line]);
+    auto fc_i = std::chrono::steady_clock::now();
+    from_chars_range(toks[0], e_in[data_line]);
+    from_chars_range(toks[1], e_out[data_line]);
+    from_chars_range(toks[2], e_weight[data_line]);
+
+    // std::from_chars(toks[0].data(), toks[0].data() + toks[0].size(),
+    // e_in[data_line]); std::from_chars(toks[1].data(), toks[1].data() +
+    // toks[1].size(),                    e_out[data_line]);
+    // std::from_chars(toks[2].data(), toks[2].data() + toks[2].size(),
+    // e_weight[data_line]);
+    from_chars_time +=
+        std::chrono::duration<double>(std::chrono::steady_clock::now() - fc_i)
+            .count();
 
     data_line++;
     start = end + 1;
     end = str.find("\n", start);
   }
+  std::cout << "Number of edges " << e_in.size() << std::endl;
+  std::cout << "First edge: " << e_in[0] << " " << e_out[0] << " "
+            << e_weight[0] << std::endl;
+
+  std::cout << "Split time           (cumulative): " << split_time << std::endl;
+  std::cout << "std::from_chars time (cumulative): " << from_chars_time
+            << std::endl;
 
   return 0;
 }
